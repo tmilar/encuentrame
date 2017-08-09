937884c09778f2988e8fa57219bd52bf986a9a52
paulucci <paulucci@strands.com>
2017-08-09 02:30:11 -0300

    Home screen: check session token before showing Login screen

--- Mobile/app/screen/Home.js
+++ Mobile/app/screen/Home.js
@@ -2,15 +2,25 @@ import React, {Component} from 'react'
 import {Text, View, StyleSheet, Button, Alert, TextInput, ScrollView} from 'react-native'
 import Feed from "../component/Feed";
 import {news, users} from "../config/data";
+import SessionService from '../service/SessionService';
 
 import {Icon} from 'react-native-elements';
 import ActionButton from "react-native-action-button";
 
 export default class Home extends Component {
+
+  async componentWillMount() {
+    let sessionId = await SessionService.getSessionToken();
+    if (!sessionId) {
+      const {navigate} = this.props.navigation;
+      navigate('Login');
+    }
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <ScrollView style={{ flex: 1.8 }}>
+      <View style={{flex: 1}}>
+        <ScrollView style={{flex: 1.8}}>
           {/* TODO add styles.container for scroll view?*/}
           <Text style={styles.paragraph}>
             Bienvenido al Home!
@@ -18,7 +28,7 @@ export default class Home extends Component {
           <Feed users={users} news={news}>
           </Feed>
         </ScrollView>
-        <ActionButton style={{ flex: 0.2 }}
+        <ActionButton style={{flex: 0.2}}
                       buttonColor="rgba(231,76,60,1)"
                       onPress={() => Alert.alert("Seguime", "Seguimiento activado \uD83D\uDE00")}
                       icon={(<Icon name="person-pin-circle" color="white" size={26}/>)}>
