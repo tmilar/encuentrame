bbf276e36eca51a8ce2b9009d8732b11872abe29
Tomas Milar <milar@strands.com>
2017-09-06 18:47:11 -0300

    Integrate native-base dependency.

--- Mobile/App.js
+++ Mobile/App.js
@@ -4,6 +4,14 @@ import {View} from "react-native";
 import {containers} from './app/style';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
+
   render() {
     return (
       <View style={[{flex: 1}, containers.statusBar]}>
