235c447f2f342901625dd9f8b482bbbd8b98244b
Tomas Milar <milar@strands.com>
2017-09-06 00:23:35 -0300

    initial commit - style/containers.js for common container styles

---
+++ Mobile/app/style/containers.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+const containers = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+export default containers;
