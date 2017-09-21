b86f99cdfd4b9321646021b857830834d5c6be98
paulucci <paulucci@strands.com>
2017-09-21 16:31:33 -0300

    Initial view for new activity screen

--- Mobile/app/screen/NewActivity.js
+++ Mobile/app/screen/NewActivity.js
@@ -1,24 +1,24 @@
 import React, {Component} from 'react';
 import {Alert, Modal, StyleSheet, Text, View, Picker, TextInput} from 'react-native';
+import {text} from '../style';
 
-export default class NewActivity extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const NewActivity = React.createClass({
+
+  getInitialState () {
+    return {
       activityName: "",
-      selectedEventId: -1
+      selectedEventId: 0,
+      selectedEventName: "Selecciona un evento"
     };
-
-    this._handleActivitynameTextChange = this._handleActivitynameTextChange.bind(this);
-  }
+  },
 
   setModalVisible(visible) {
     this.setState({modalVisible: visible});
-  }
+  },
 
   _handleActivitynameTextChange(inputValue) {
     this.setState({activityName: inputValue})
-  }
+  },
 
   /*_backToHome() {
     this.setModalVisible(false);
@@ -27,10 +27,9 @@ export default class NewActivity extends Component {
 
   componentDidMount() {
     this.setModalVisible(!this.state.modalVisible)
-  }
+  },
 
   render() {
-
     return (
       <View style={{marginTop: 22}}>
         <Modal
@@ -40,35 +39,41 @@ export default class NewActivity extends Component {
           onRequestClose={() => {/* handle modal 'back' close? */
           }}
         >
-          <View style={styles.message}>
-            <View>
-
-              <Text>Nueva Actividad</Text>
+          <View style={{flex: 1}}>
+            <Text style={[text.p, styles.activityTitle]}>
+              Nueva Actividad
+            </Text>
+            <View style={{flex: 3, flexDirection:'column', justifyContent:'flex-start', alignItems: "center"}}>
+              <TextInput
+                value={this.state.activityName}
+                placeholder="Nombre de la actividad"
+                ref="activityName"
+                style={styles.activityName}
+                selectTextOnFocus
+                onChangeText={this._handleActivitynameTextChange}
+              />
+              <Text style={text.p}>
+                {this.state.selectedEventName || "Si vas a un evento eligelo"}
+              </Text>
+              <Picker
+                selectedValue={this.state.selectedEventId}
+                style={styles.picker}
+                onValueChange={(itemValue, itemIndex) => this.setState({selectedEventId: itemValue})}
+                color= "red"
+              >
+                <Picker.Item label="Recital del Indio" value="1"/>
+                <Picker.Item label="Boca-RiBer" value="2"/>
+              </Picker>
 
             </View>
           </View>
-          <View style={{flex: 1}}>
-            <TextInput
-              value={this.state.activityName}
-              placeholder="Nombre de la actividad"
-              ref="activityName"
-              style={styles.activityName}
-              selectTextOnFocus
-              onChangeText={this._handleActivitynameTextChange}
-            />
-            <Picker
-              selectedValue={this.state.selectedEventId}
-              onValueChange={(itemValue, itemIndex) => this.setState({selectedEventId: itemValue})}>
-              <Picker.Item label="Recital del Indio" value="1" />
-              <Picker.Item label="Boca-RiBer" value="2" />
-            </Picker>
-          </View>
+
         </Modal>
 
       </View>
     )
   }
-}
+});
 
 const styles = StyleSheet.create({
   message: {
@@ -76,5 +81,24 @@ const styles = StyleSheet.create({
     height: 3,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  picker: {
+    width: 200,
+    paddingBottom: 10
+  },
+  activityName: {
+    width: 200,
+    textAlign: 'center',
+    paddingBottom: 10
+  },
+  activityTitle: {
+    backgroundColor: "#2962FF",
+    color: "white",
+    flex: 1,
+    width: 400,
+    alignSelf: "center",
+    textAlignVertical: "center"
   }
 });
+
+export default NewActivity;
