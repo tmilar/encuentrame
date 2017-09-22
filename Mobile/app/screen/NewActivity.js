26707d1a2127cfdf721da31757831d75d700b771
paulucci <paulucci@strands.com>
2017-09-21 22:39:11 -0300

    Improving new activity view

--- Mobile/app/screen/NewActivity.js
+++ Mobile/app/screen/NewActivity.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Alert, Modal, StyleSheet, Text, View, Picker, TextInput} from 'react-native';
+import {Alert, Modal, StyleSheet, Text, View, Picker, TextInput, Button} from 'react-native';
 import {text} from '../style';
 import { MapView } from 'expo';
 
@@ -20,6 +20,17 @@ const NewActivity = React.createClass({
   _handleActivitynameTextChange(inputValue) {
     this.setState({activityName: inputValue})
   },
+  _handleCreateActivityButtonPress() {
+    //TODO retrieve and save activity data
+    this._goToHome();
+  },
+  _handleCancelActivityCreation() {
+    this._goToHome();
+  },
+  _goToHome() {
+    this.setModalVisible(false);
+    this.props.navigation.goBack(null);
+  },
 
   /*_backToHome() {
     this.setModalVisible(false);
@@ -31,6 +42,42 @@ const NewActivity = React.createClass({
   },
 
   render() {
+    var mapStyles = [
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ];
     return (
       <View style={{marginTop: 22}}>
         <Modal
@@ -44,7 +91,7 @@ const NewActivity = React.createClass({
             <Text style={[text.p, styles.activityTitle]}>
               Nueva Actividad
             </Text>
-            <View style={{flex: 3, flexDirection:'column', justifyContent:'flex-start', alignItems: "center"}}>
+            <View style={{flex: 2, flexDirection:'column', justifyContent:'flex-start', alignItems: "center"}}>
               <TextInput
                 value={this.state.activityName}
                 placeholder="Nombre de la actividad"
@@ -65,14 +112,36 @@ const NewActivity = React.createClass({
                 <Picker.Item label="Recital del Indio" value="1"/>
                 <Picker.Item label="Boca-RiBer" value="2"/>
               </Picker>
-              <MapView style={styles.map}
-                initialRegion={{
-                  latitude: 37.78825,
-                  longitude: -122.4324,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }}
-              />
+
+            </View>
+            <Text style={text.p}>
+              Ubicacion de la actividad?
+            </Text>
+            <MapView style={styles.map}
+                     customMapStyle={mapStyles}
+                     initialRegion={{
+                       latitude: 37.78825,
+                       longitude: -122.4324,
+                       latitudeDelta: 0.0922,
+                       longitudeDelta: 0.0421,
+                     }}
+            />
+            <View style={[styles.footer, {flexDirection: "row", justifyContent: "center" , flexWrap: "wrap"}]}>
+              <View style={{width: 300 , flex: 1}}>
+                <Button
+                  title="Crear Actividad"
+                  onPress={this._handleCreateActivityButtonPress}
+                />
+              </View>
+
+              <View style={{width: 300 , flex: 1}}>
+                <Button
+                  color="grey"
+                  title="Cancelar"
+                  onPress={this._handleCancelActivityCreation}
+                />
+              </View>
+
             </View>
           </View>
 
@@ -108,11 +177,8 @@ const styles = StyleSheet.create({
     textAlignVertical: "center"
   },
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    flex: 3,
+    margin: 50
   }
 });
 
