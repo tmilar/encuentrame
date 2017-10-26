dbf2ef3799580033338d6c30537562e706a4fd36
Tomas Milar <milar@strands.com>
2017-10-25 23:37:36 -0300

    SoughtPersonService  initial implementation.
    	Working methods:
    			* getSoughtPeople()
    			* soughtPersonSupplyInfo()
    	Waiting for backend implementation:
    			* soughtPersonDismiss()

---
+++ Mobile/app/service/SoughtPeopleService.js
@@ -0,0 +1,49 @@
+import Service from './Service';
+
+class SoughtPeopleService {
+
+  /**
+   * Get sought people list relevant for me.
+   *
+   * @returns {Promise.<*>}
+   */
+  getSoughtPeople = async () => {
+    let url = "/soughtPeople";
+    let soughtPeople = await Service.sendRequest(url);
+    //TODO request all people images here?
+    console.debug(`[SoughtPeopleService] Received ${soughtPeople.length} sought people.`);
+    return soughtPeople;
+  };
+
+  /**
+   * Send sought person info.
+   *
+   * @param soughtPersonId
+   * @param info {latitude, longitude, info}
+   * @returns {Promise.<void>}
+   */
+  soughtPersonSupplyInfo = async (soughtPersonId, {latitude, longitude, info}) => {
+    let url = `/soughtPerson/seen/${soughtPersonId}`;
+
+    return await Service.sendRequest(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          "Latitude": latitude,
+          "Longitude": longitude,
+          "Info": info,
+        })
+      }
+    )
+  };
+
+  /**
+   * Dismiss a sought person request.
+   *
+   * @param soughtPersonId
+   * @returns {Promise.<void>}
+   */
+  soughtPersonDismiss = async (soughtPersonId) => {
+    console.log(`[SoughtPeopleService] Dismissing sought person ${soughtPersonId}`);
+    // TODO send request to server
+  }
+}
