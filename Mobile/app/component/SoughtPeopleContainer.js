d8073b11cb50b69ad27410e04859789ec5460147
Tomas Milar <milar@strands.com>
2017-10-26 03:01:34 -0300

    Integrate SoughtPeopleContainer data source with SoughtPeopleService.

--- Mobile/app/component/SoughtPeopleContainer.js
+++ Mobile/app/component/SoughtPeopleContainer.js
@@ -2,15 +2,16 @@ import React, {Component} from 'react';
 import SoughtPeopleDeckSwiper from './SoughtPeopleDeckSwiper';
 import LoadingIndicator from "./LoadingIndicator";
 import {soughtPeople} from "../config/soughtPeopleFixture";
-
+import SoughtPeopleService from "../service/SoughtPeopleService";
 
 export default class SoughtPeopleContainer extends Component {
 
   soughtPeople = [];
 
   componentWillMount = async () => {
+    this.soughtPeople = await SoughtPeopleService.getSoughtPeople();
     // TODO remove soughtPeople fixtures when backend is working.
-    this.soughtPeople = soughtPeople;
+    this.soughtPeople = this.soughtPeople || soughtPeople;
   };
 
   render() {
