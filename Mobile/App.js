1297d33d99f5d2623524f127ad5e6ed088292323
Tomas Milar <milar@strands.com>
2017-10-26 01:26:19 -0300

    Integrate native-base v2.3.3 dependency.

--- Mobile/App.js
+++ Mobile/App.js
@@ -5,6 +5,12 @@ import {containers} from './app/style';
 
 class App extends Component {
 
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
 
   render() {
     return (
