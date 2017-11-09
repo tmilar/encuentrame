ffef321e6fcc12f875cbf71f622fea7efb84306f
Tomas Milar <milar@strands.com>
2017-11-09 20:21:17 -0300

    New SupplyInfo component: initial display of question answers.

--- Mobile/app/component/supplyInfo/SupplyInfo.js
+++ Mobile/app/component/supplyInfo/SupplyInfo.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
-import {View, StyleSheet, Alert} from "react-native";
+import {View, StyleSheet, Alert, Text} from "react-native";
 import TabProgressTracker from "./TabProgressTracker";
 
-/**
- * - get the questions
- * - build the views
- * - pass them to the progress tracker
- */
+//
+// - get the questions
+// - build the views
+// - pass them to the progress tracker
+//
 export default class SupplyInfo extends Component {
 
   state = {
@@ -18,18 +18,32 @@ export default class SupplyInfo extends Component {
   };
 
   renderCurrentQuestionView() {
-    let currentQuestion = this.props.questions[this.state.currentQuestionIndex];
+    let questionsCount = this.props.questions.length;
+    let currentIndex = this.state.currentQuestionIndex;
+    //TODO remove this and replace with actual answers.
+    return <Text>No quedan preguntas por responder.</Text>;
 
     // TODO render the current question: answers, text.
   }
 
   render() {
-    return <View>
+    return <View style={styles.container}>
       <TabProgressTracker
         items={this.props.questions.map(q => q.text)}
         selectedIndex={this.state.currentQuestionIndex}
       />
-      {this.renderCurrentQuestionView()}
+      <View style={{flex: 1}}>
+        {this.renderCurrentQuestionView()}
+      </View>
     </View>
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D2D100', // '#D2D100', //#ecf0f1',
+  }
+});
