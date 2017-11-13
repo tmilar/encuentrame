c379991f4124c029199fe555997cfe151c7edbc1
Tomas Milar <milar@strands.com>
2017-11-12 21:45:58 -0300

    Contacts screen: remove 'Contactos' word from view.

--- Mobile/app/component/FamilyListContainer.js
+++ Mobile/app/component/FamilyListContainer.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native'
 import FamilyList from "./FamilyList";
-import {familyMembers} from "../config/familyFixture";
-import {text} from '../style';
 import ContactsService from '../service/ContactsService';
 
 export default class FamilyListContainer extends Component {
@@ -15,18 +13,14 @@ export default class FamilyListContainer extends Component {
     this.setState({contacts});
   };
 
-  fetchContacts = async () =>  {
+  fetchContacts = async () => {
     let contacts = await ContactsService.getAllContacts();
     return contacts;
-  }
+  };
 
   render() {
-    return <View style={{flex: 1}}>
-            <Text style={text.p}>
-              Contactos
-            </Text>
-            <FamilyList familyMembers={this.state.contacts}/>
-          </View>
-   ;
+    return <View style={{flex: 1, marginTop: 5}}>
+      <FamilyList familyMembers={this.state.contacts}/>
+    </View>
   }
 }
